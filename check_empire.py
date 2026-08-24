50e0d0a7d4fe4fa56a723848c7d36899118959f7
onionize <onionize@localhost>
2026-08-24 11:46:36 +0100

    Remove trash file

--- check_empire.py
+++
@@ -1,5 +0,0 @@
-# check_empire.py
-import requests, re
-r = requests.get('https://aionempire.com/', headers={'User-Agent': 'Mozilla/5.0'})
-idx = r.text.find('class="online"')
-print(repr(r.text[idx:idx+200]))
